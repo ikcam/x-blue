c7610472d023a0d2389f42653c68499ddba452d7
Irving Kcam <xploit29@gmail.com>
2012-11-11 01:58:31 -0500

    Prueba

--- functions.php
+++ functions.php
@@ -602,6 +602,9 @@ function sandbox_excerpt($text) {
 remove_filter('get_the_excerpt', 'wp_trim_excerpt');
 add_filter( 'get_the_excerpt', 'sandbox_excerpt');
 
-/* COMENTARIO DE PRUEBA */
+/* COMENTARIO DE PRUEBA 2 */
+/* COMENTARIO DE PRUEBA 2 */
+/* COMENTARIO DE PRUEBA 2 */
+/* COMENTARIO DE PRUEBA 2 */
 
 ?>
