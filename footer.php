b89b3ee61434d3a44b39d7633c2bff597a3b0caa
Irving Kcam <xploit29@gmail.com>
2012-11-11 02:50:07 -0500

    Test

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 
 	<footer id="footer">
+		<?php wp_footer() ?>
 		<span id="generator-link"><a href="http://wordpress.org/" title="<?php _e( 'WordPress', 'sandbox' ) ?>" rel="generator"><?php _e( 'WordPress', 'sandbox' ) ?></a></span>
 		<span class="meta-sep">|</span>
 		<span id="theme-link"><a href="http://www.plaintxt.org/themes/sandbox/" title="<?php _e( 'Sandbox theme for WordPress', 'sandbox' ) ?>" rel="designer"><?php _e( 'Sandbox', 'sandbox' ) ?></a></span>
@@ -7,7 +8,6 @@
 		<span id="theme-link">Improved by <a href="http://ikcam.com" title="Irving Kcam">@iKcam</a>
 		<span class="meta-sep">|</span>
 		<span id="theme-link"><a href="https://github.com/ikcam/x-blue" rel="GitHub">GitHub</a></span>	
-		<?php wp_footer() ?>
 	</footer><!-- #footer -->
 
 </section><!-- #wrapper .hfeed -->
